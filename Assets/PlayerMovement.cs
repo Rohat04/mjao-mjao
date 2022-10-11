8321b691dc17262d0b8866ea5be529311e3dbf02
Rohat04 <ozkocrohat@gmail.com>
2022-10-11 12:49:04 +0200

    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.7f;
+    public LayerMask groundMask;
+
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        // print(isGrounded);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        //Bruh
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
+
+
+
+
+
+
+
+
+/*
+         if(Input.GetButton("<Your button for sprinting>"))
+         {
+         speed = runSpeed; 
+         }
+         else 
+         { 
+             speed = walkSpeed; 
+         }
+*/
+/*
+Based off of BreadNotSeen's sprint code, I was able to add "sneak"! (Add under Sprint or Jump command)
+       
+if (Input.GetKey("left ctrl") && isGrounded)
+        {
+            speed = 0.5f;
+        }
+        else
+        {
+            speed = 3f;
+        }
+*/
